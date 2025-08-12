b945f40492a40b393ad69ccaded4545e399e523c
ericp <137734293+erpzz@users.noreply.github.com>
2025-08-12 18:31:20 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,18 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       {/* Standard favicon */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="public/favicon.ico" sizes="any" />
 
         {/* PNG favicons */}
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 
         {/* Apple Touch icon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="public/apple-touch-icon.png" />
 
         {/* Android/Chrome icons */}
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="public/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="public/android-chrome-512x512.png" />
 
         {/* Optional: theme color for mobile */}
         <meta name="theme-color" content="#00ff00" />
