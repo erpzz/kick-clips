1879fb724d7f00e84e6be6b27670e748a2997479
yaahboii <137734293+yaahboii@users.noreply.github.com>
2025-08-12 18:07:45 -0400

    Update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      {/* Standard favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        {/* PNG favicons */}
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+        {/* Apple Touch icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+        {/* Android/Chrome icons */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+
+        {/* Optional: theme color for mobile */}
+        <meta name="theme-color" content="#00ff00" />
       <Head />
       <body>
         <Main />
